2be06b60199b645da7698c2191390a47f88e4610
evillgranny <tractor2554393@yandex.by>
2021-10-12 02:40:33 +0200

    first commit

---
+++ lib/pages/main_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Список дел'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Column(
+        children: [
+          Text('Main Screen', style: TextStyle(
+            color: Colors.white
+          )),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamedAndRemoveUntil(context, '/todo', (route) => false);
+            },
+            child: Text('Перейти далее'),
+          )
+        ]
+      )
+    );
+  }
+}
